da9c84a766f388377fb078a6f1c7d3ad47e9fcff
Ansi_Nitro <15asktt@gmail.com>
2023-04-16 09:18:03 +0600

    MyArrayList in full complectation)

--- src/week3/assigment2/classes/MyArrayList.java
+++ src/week3/assigment2/classes/MyArrayList.java
@@ -6,7 +6,8 @@ import java.util.Iterator;
 
 public class MyArrayList<T> implements MyList<T> {
     private Object[] hiddenArr;
-    private int length;
+    private int capacity = 5;
+    private int size;
     public MyArrayList() {
         this(5);
     }
@@ -16,89 +17,87 @@ public class MyArrayList<T> implements MyList<T> {
     }
 
     private void increaseBuffer() {
-        int biggerSize = (int)(hiddenArr.length * 1.5);
-        Object[] tempArr = new Object[biggerSize];
+        int newCapacity = capacity * 2;
+        Object[] tempArr = new Object[newCapacity];
 
-        for (int i = 0; i < hiddenArr.length; i++)
+        for (int i = 0; i < capacity; i++)
             tempArr[i] = hiddenArr[i];
 
         hiddenArr = tempArr;
     }
 
-    private void swap(T a, T b){
-        T temp = a;
-        a = b;
-        b = temp;
+    private void swap(Object[] arr, int i, int j){
+        Object temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
     @Override
     public int size() {
-        return length;
+        return size;
     }
 
     @Override
     public boolean contains(Object o) {
-        for (int i = 0; i < length; i++)
-            if (hiddenArr[i] == o)
-                return true;
-
-        return false;
+        return indexOf(o) > 0;
     }
 
     @Override
     public void add(T item) {
-        if (length == hiddenArr.length)
+        if (size == capacity)
             increaseBuffer();
-        hiddenArr[length++] = item;
+        hiddenArr[size++] = item;
     }
 
     @Override
     public void add(T item, int index) {
-        if (length == hiddenArr.length)
+        if (index < 0 || index > size)
+            throw new IndexOutOfBoundsException();
+        if (size == capacity)
             increaseBuffer();
-        for (int i = length - 1; i >= index; i--)
+        for (int i = size - 1; i >= index; i--)
             hiddenArr[i + 1] = hiddenArr[i];
         hiddenArr[index] = item;
-        length++;
+        size++;
     }
 
     @Override
     public boolean remove(T item) {
-        for (int i = 0; i < length; i++)
-            if (hiddenArr[i] == item) {
-                for (int j = i; j < length - 1; j++)
-                    hiddenArr[i] = hiddenArr[i + 1];
-                return true;
-            }
-
-        return false;
+        int index = indexOf(item);
+        if (index < 0)
+            return false;
+        remove(index);
+        return true;
     }
 
     @Override
     public T remove(int index) {
+        if (index < 0 || index > size)
+            throw new IndexOutOfBoundsException();
         T temp = get(index);
-        for (int i = index; i < length - 1; i++)
+        for (int i = index; i < size - 1; i++)
             hiddenArr[i] = hiddenArr[i + 1];
-        length--;
+        size--;
 
         return temp;
     }
 
     @Override
     public void clear() {
-        length = 0;
+        hiddenArr = new Object[5];
+        size = 0;
     }
 
     @Override
     public T get(int index) {
-        if(index >= length)
+        if (index < 0 || index > size)
             throw new IndexOutOfBoundsException();
         return (T) hiddenArr[index];
     }
 
     @Override
     public int indexOf(Object o) {
-        for (int i = 0; i < length; i++)
+        for (int i = 0; i < size; i++)
             if (hiddenArr[i] == o)
                 return i;
 
@@ -107,7 +106,7 @@ public class MyArrayList<T> implements MyList<T> {
 
     @Override
     public int lastIndexOf(Object o) {
-        for (int i = length - 1; i > 0; i--)
+        for (int i = size - 1; i > 0; i--)
             if (hiddenArr[i] == o)
                 return i;
 
@@ -116,10 +115,10 @@ public class MyArrayList<T> implements MyList<T> {
 
     @Override
     public void sort() {
-        for (int i = 0; i < size() - 1; i++)
-            for (int j = 0; j < size() - i - 1; j++)
-                if (hiddenArr[j] < hiddenArr[j + 1])
-                    swap(hiddenArr[j], hiddenArr[j + 1]);
+        for (int i = 0; i < size - 1; i++)
+            for (int j = 0; j < size - i - 1; j++)
+                if (((Comparable<T>) hiddenArr[j]).compareTo((T) hiddenArr[j + 1]) > 0)
+                    swap(hiddenArr, j, j + 1);
     }
 
     @Override
